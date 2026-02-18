43cd614aee8948c71ee04d2af18802ee59aa9fcf
nathan-m-1 <seromgn@hotmail.com>
2026-02-17 22:13:36 -0600

    Se implementa un web socket para un chat a travez de una aplicacion web.  El codigo fue generado con ayuda del agente de Google Antigravity

---
+++ src/test/java/com/network/protocols/MainTest.java
@@ -0,0 +1,13 @@
+package com.network.protocols;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        System.out.println("Hello World");
+        assertTrue(true);
+    }
+}
